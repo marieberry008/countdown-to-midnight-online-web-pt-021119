dceade67c92b867e55a57b0c1c2ec67b09045004
Marie Berry (Leugermann) <marie@creativeos.io>
2019-02-24 21:52:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,11 @@
+def countdown(integer)
+  integer = 10
+  while integer > 0
+  puts "#{integer} SECOND(S)!"
+  integer -= 1
+  end
+"HAPPY NEW YEAR!"
+end
 def countdown_with_sleep(integer)
   integer = 10
   while integer > 0
